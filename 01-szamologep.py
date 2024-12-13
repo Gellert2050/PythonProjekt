3d7ab2674ebf71cbdf814bc389c74002753f15d1
ok-okozat <ok-okozat@gmail.com>
2024-12-13 14:56:52 +0100

    W

--- 01-szamologep.py
+++ 01-szamologep.py
@@ -3,16 +3,16 @@ m = input("Művelet jel: ")
 sz2 = int(input("Második szám: ")) 
 
 if sz2 == 0 and m == "/":
-    print("Nem lehet nullával osztani.")
+    e = "nullával való osztás"
+elif m == "+":
+    e = sz1+sz2
+elif m == "-":
+     e = sz1-sz2
+elif m == "*":
+    e = sz1*sz2
+elif m == "/":
+    e = sz1/sz2
 else:
-    if m == "+":
-        e = sz1+sz2
-    elif m == "-":
-         e = sz1-sz2
-    elif m == "*":
-        e = sz1*sz2
-    elif m == "/":
-        e = sz1/sz2
-    else:
-        e = "ismeretlen"
-    print(f"{sz1} {m} {sz2} = {e}")
+    e = "ismeretlen"
+
+print(f"{sz1} {m} {sz2} = {e}")
